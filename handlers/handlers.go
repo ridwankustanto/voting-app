f57b3aabbc583a52d0cc2a1b71617c8b189784ae
Ridwan Kustanto <ridwankustanto@gmail.com>
2018-11-23 12:31:03 +0700

    RK: created models that take care of query to db

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,18 +4,21 @@ import (
 	"database/sql"
 	"net/http"
 	"strconv"
+	"voting-app/models"
 
 	"github.com/labstack/echo"
 )
 
 // Handler for get all polls
 
+// GetPolls to handle response get poll
 func GetPolls(db *sql.DB) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		return c.JSON(http.StatusOK, models.GetPolls(db))
 	}
 }
 
+// UpdatePoll to handle update poll
 func UpdatePoll(db *sql.DB) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		var poll models.Poll
@@ -27,9 +30,7 @@ func UpdatePoll(db *sql.DB) echo.HandlerFunc {
 		id, err := models.UpdatePoll(db, index, poll.Name, poll.Upvotes, poll.Downvotes)
 
 		if err == nil {
-			return c.JSON(http.StatusCreated, H{
-				"affected": id,
-			})
+			return c.JSON(http.StatusCreated, id)
 		}
 
 		return err
